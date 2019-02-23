032a934ffd1cf8cc4bc9404f50b00c43e1b692e0
arqarq <43341071+arqarq@users.noreply.github.com>
2019-02-23 11:20:25 +0100

    - Singleton & Prototype

--- hellospringjavaconf/src/main/java/pl/sdacademy/HelloSpringApp.java
+++ hellospringjavaconf/src/main/java/pl/sdacademy/HelloSpringApp.java
@@ -8,8 +8,18 @@ public class HelloSpringApp {
 //        ApplicationContext context = new AnnotationConfigApplicationContext(HelloSpringConfig.class);
         ApplicationContext context = SpringApplication.run(HelloSpringConfig.class);
 
-//        HelloSpring bean = (HelloSpring) context.getBean("helloSpring");
-        HelloSpring bean = context.getBean(HelloSpring.class);
+        HelloSpring bean = (HelloSpring) context.getBean("helloSpring");
+//        HelloSpring bean = context.getBean(HelloSpring.class);
         System.out.println(bean.getWelcomeMessage());
+
+        HelloSpring beanS1 = (HelloSpring) context.getBean("helloSingletonGreeter");
+        HelloSpring beanS2 = (HelloSpring) context.getBean("helloSingletonGreeter");
+        System.out.println(beanS1.getWelcomeMessage());
+        HelloSpring beanP1 = (HelloSpring) context.getBean("helloPrototypeGreeter");
+        HelloSpring beanP2 = (HelloSpring) context.getBean("helloPrototypeGreeter");
+        System.out.println(beanP1.getWelcomeMessage());
+
+        System.out.println("comparing singletons: " + beanS1.equals(beanS2));
+        System.out.println("comparing prototypes: " + beanP1.equals(beanP2));
     }
 }
--- hellospringjavaconf/src/main/java/pl/sdacademy/HelloSpringConfig.java
+++ hellospringjavaconf/src/main/java/pl/sdacademy/HelloSpringConfig.java
@@ -2,6 +2,7 @@ package pl.sdacademy;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class HelloSpringConfig {
@@ -10,6 +11,24 @@ public class HelloSpringConfig {
         HelloSpring helloSpring = new HelloSpring();
 
         helloSpring.setWelcomeMessage("Hello Spring!!!");
+        return helloSpring;
+    }
+
+    @Bean
+    @Scope("singleton") // domyślny
+    public HelloSpring helloSingletonGreeter() {
+        HelloSpring helloSpring = new HelloSpring();
+
+        helloSpring.setWelcomeMessage("Hello from singleton!!!");
+        return helloSpring;
+    }
+
+    @Bean
+    @Scope("prototype")
+    public HelloSpring helloPrototypeGreeter() {
+        HelloSpring helloSpring = new HelloSpring();
+
+        helloSpring.setWelcomeMessage("Hello from prototype!!!");
         return helloSpring;
     }
 }
