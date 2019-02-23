b8380dce7b53b864d953aede0413dffb787fb3e1
arqarq <43341071+arqarq@users.noreply.github.com>
2019-02-23 11:25:41 +0100

    - Singleton & Prototype /XML

--- hellospringxml/src/main/java/pl/sdacademy/HelloSpringApp.java
+++ hellospringxml/src/main/java/pl/sdacademy/HelloSpringApp.java
@@ -7,8 +7,18 @@ public class HelloSpringApp {
     public static void main(String[] args) {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("beans.xml");
-        HelloSpring bean = (HelloSpring) context.getBean("helloSpring");
 
+        HelloSpring bean = (HelloSpring) context.getBean("helloSpring");
         System.out.println(bean.getWelcomeMessage());
+
+        HelloSpring beanS1 = (HelloSpring) context.getBean("helloSingletonGreeter");
+        HelloSpring beanS2 = (HelloSpring) context.getBean("helloSingletonGreeter");
+        System.out.println(beanS1.getWelcomeMessage());
+        HelloSpring beanP1 = (HelloSpring) context.getBean("helloPrototypeGreeter");
+        HelloSpring beanP2 = (HelloSpring) context.getBean("helloPrototypeGreeter");
+        System.out.println(beanP1.getWelcomeMessage());
+
+        System.out.println("comparing singletons: " + beanS1.equals(beanS2));
+        System.out.println("comparing prototypes: " + beanP1.equals(beanP2));
     }
 }
