e0646bfe9aec45d91bef2630e156d88fd3a3ede6
arqarq <43341071+arqarq@users.noreply.github.com>
2019-02-24 14:09:28 +0100

    - zad. 6 (5 with tests)

--- spring-boot-5/src/test/java/pl/sdacademy/springboot5/SpringBoot5ApplicationTests.java
+++ spring-boot-5/src/test/java/pl/sdacademy/springboot5/SpringBoot5ApplicationTests.java
@@ -2,15 +2,24 @@ package pl.sdacademy.springboot5;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBoot5ApplicationTests {
+    @Autowired
+    private GreetingController greetingController;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
+    @Test
+    public void shouldBeanNotBeNull() {
+        assertThat(greetingController).isNotNull();
+    }
 }
