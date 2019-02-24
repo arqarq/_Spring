295bcf8907904e2a50e23b7a26a95bc3f01daf51
arqarq <43341071+arqarq@users.noreply.github.com>
2019-02-24 11:56:23 +0100

    - zad. 4B done, with simple teapot response

--- spring-boot-4B/src/main/java/pl/sdacademy/springboot4B/MyController.java
+++ spring-boot-4B/src/main/java/pl/sdacademy/springboot4B/MyController.java
@@ -1,6 +1,9 @@
 package pl.sdacademy.springboot4B;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.logging.Logger;
@@ -10,7 +13,10 @@ public class MyController {
     private static final Logger LOG = Logger.getLogger(SpringBoot4BApplication.class.getName());
 
     @RequestMapping("/")
-    public void printHello() {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    public String printHello() {
         LOG.info("Hello Spring Boot! It’s really awesome!");
+        return "Hello Spring Boot! It’s really awesome!";
     }
 }
