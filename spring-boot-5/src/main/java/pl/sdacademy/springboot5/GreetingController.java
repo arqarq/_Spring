70d7ba712f8283e3b26ec66472643f9627dc1d9e
arqarq <43341071+arqarq@users.noreply.github.com>
2019-02-24 13:19:37 +0100

    - zad. 5 end

--- spring-boot-5/src/main/java/pl/sdacademy/springboot5/GreetingController.java
+++ spring-boot-5/src/main/java/pl/sdacademy/springboot5/GreetingController.java
@@ -1,12 +1,18 @@
 package pl.sdacademy.springboot5;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-    @GetMapping("/greeting")
-    public String greeting() {
-        return "greet";
+    @GetMapping("/greetingEndpoint")
+    public String greeting(Model model,
+                           @RequestParam(name = "additional", required = false, defaultValue = "abcdef")
+                                   String additional) {
+        model.addAttribute("myAttribute", "I'm set in the controller");
+        model.addAttribute("additionalAttribute", additional);
+        return "greeting-view";
     }
 }
