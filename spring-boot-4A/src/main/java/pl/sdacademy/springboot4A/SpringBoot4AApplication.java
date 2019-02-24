87705aa89222eef57f260d15cbba867762c86264
arqarq <43341071+arqarq@users.noreply.github.com>
2019-02-24 10:29:25 +0100

    - zad. 4A start

---
+++ spring-boot-4A/src/main/java/pl/sdacademy/springboot4A/SpringBoot4AApplication.java
@@ -0,0 +1,11 @@
+package pl.sdacademy.springboot4A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot4AApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot4AApplication.class, args);
+    }
+}
---
+++ spring-boot-4A/src/test/java/pl/sdacademy/springboot4A/SpringBoot4AApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.sdacademy.springboot4A;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot4AApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
