44c05eea09cdfcad89be2b6b8fd52e9f900e3251
arqarq <43341071+arqarq@users.noreply.github.com>
2019-03-02 13:57:35 +0100

    - test

--- spring-jdbc/src/test/java/pl/sdacademy/springjdbc/PersonRepositoryTest.java
+++ spring-jdbc/src/test/java/pl/sdacademy/springjdbc/PersonRepositoryTest.java
@@ -3,14 +3,9 @@ package pl.sdacademy.springjdbc;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-import pl.sdacademy.springjdbc.PersonRepository;
-import pl.sdacademy.springjdbc.PersonRepositoryConfig;
-import pl.sdacademy.springjdbc.SpringJdbcApplication;
 import pl.sdacademy.springjdbc.model.Person;
 import pl.sdacademy.springjdbc.model.Profession;
 
