b9dab1c8ff095eda6ccb60062bbcc6e52a630b40
arqarq <43341071+arqarq@users.noreply.github.com>
2019-03-02 11:12:59 +0100

    - conf

--- spring-jdbc/src/main/java/pl/sdacademy/springjdbc/SpringJdbcApplication.java
+++ spring-jdbc/src/main/java/pl/sdacademy/springjdbc/SpringJdbcApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcApplication.class, args);
+    }
 }
