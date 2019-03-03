bf79050ae1550f97f8d13df98ebe60b0424a06be
arqarq <43341071+arqarq@users.noreply.github.com>
2019-03-03 13:56:04 +0100

    - zad. 5A tests - delete TODO

--- spring-mongodb/src/test/java/pl/sdacademy/springmongodb/SpringMongodbApplicationTests.java
+++ spring-mongodb/src/test/java/pl/sdacademy/springmongodb/SpringMongodbApplicationTests.java
@@ -1,11 +1,15 @@
 package pl.sdacademy.springmongodb;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.sdacademy.springmongodb.model.Car;
 import pl.sdacademy.springmongodb.repositories.CarRepository;
@@ -29,12 +33,11 @@ public class SpringMongodbApplicationTests {
 
     @Before
     public void init_() {
-        mongoTemplate.dropCollection("cars");
         mongoTemplate.createCollection("cars");
 
         car = Car.builder()
-                .brand("Enzo")
-                .name("Fiatarri")
+                .brand("Fiatarri")
+                .name("Enzo")
                 .capacity(6500)
                 .hp(200)
                 .build();
@@ -47,12 +50,49 @@ public class SpringMongodbApplicationTests {
                 .hp(50)
                 .build();
         mongoTemplate.insert(car, "cars");
+
+        car = Car.builder()
+                .brand("Peugeot")
+                .name("407")
+                .capacity(1597)
+                .hp(110)
+                .build();
+        mongoTemplate.insert(car, "cars");
+    }
+
+    @After
+    public void end_() {
+        mongoTemplate.dropCollection("cars");
     }
 
     @Test
     public void shouldTestMongoTemplate() {
         System.out.println("============= MongoTemplate");
-        mongoTemplate.findAll(Car.class, "cars").forEach(doc -> System.out.println(doc.toString()));
+        List<Car> cars = mongoTemplate.findAll(Car.class, "cars");
+        cars.forEach(doc -> System.out.println(doc.toString()));
+
+        Query query = new Query().addCriteria(Criteria.where("brand").is("Fiat"));
+        List<Car> foundDocs = mongoTemplate.find(query, Car.class);
+        assertThat(foundDocs.size()).isOne();
+        assertThat(mongoTemplate.count(query, "cars")).isOne();
+
+        foundDocs = mongoTemplate.findAll(Car.class);
+        assertThat(foundDocs.size()).isEqualTo(3);
+
+        query = new Query().addCriteria(Criteria.where("brand").regex("Fiat.*"));
+        foundDocs = mongoTemplate.find(query, Car.class);
+        assertThat(foundDocs.size()).isEqualTo(2);
+
+        Query queryToDel = new Query().addCriteria(Criteria.where("brand").regex("Fiatarri"));
+        mongoTemplate.remove(queryToDel, Car.class);
+        assertThat(mongoTemplate.count(query, "cars")).isOne();
+        foundDocs = mongoTemplate.find(query, Car.class);
+        assertThat(foundDocs.size()).isOne();
+    }
+
+    @Test
+    @Ignore
+    public void testDeleteInMongoDB() {
     }
 
     @Test
