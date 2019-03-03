33b25b15a7a1b18894fdc2e3dc384d94a7859a08
arqarq <43341071+arqarq@users.noreply.github.com>
2019-03-03 11:13:51 +0100

    - zad. 5A (mongoDB) started

---
+++ spring-mongodb/src/main/java/pl/sdacademy/springmongodb/SpringMongodbApplication.java
@@ -0,0 +1,11 @@
+package pl.sdacademy.springmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMongodbApplication.class, args);
+    }
+}
